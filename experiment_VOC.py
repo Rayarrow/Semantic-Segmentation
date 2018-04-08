21b233790e1ccaef7eacb3d72f976da0d51c7b6d
exxxi <2094167260@qq.com>
2018-04-08 09:11:14 +0800

    da_v3_variance

--- experiment_VOC.py
+++ experiment_VOC.py
@@ -19,15 +19,15 @@ seg_parser.add_argument('--resize_shape', default=224)
 
 # Learning control
 seg_parser.add_argument('--learning_rate', type=float, default=1e-4)
-seg_parser.add_argument('--lr_decay', type=str, default='poly')
+seg_parser.add_argument('--lr_decay', type=str, default='None')
 seg_parser.add_argument('--momentum', type=float, default=0.9)
 seg_parser.add_argument('--weight_decay', type=float, default=1e-4)
-seg_parser.add_argument('--nr_iter', type=int, default=130000)
-seg_parser.add_argument('--batch_size', type=int, default=4)
+seg_parser.add_argument('--nr_iter', type=int, default=80000)
+seg_parser.add_argument('--batch_size', type=int, default=6)
 seg_parser.add_argument('--is_train', action='store_true')
 seg_parser.add_argument('--is_predict', action='store_true')
-seg_parser.add_argument('--devices', type=str, default='0')
-seg_parser.add_argument('--report_interval', type=int, default=10)
+seg_parser.add_argument('--devices', type=str, default='4')
+seg_parser.add_argument('--report_interval', type=int, default=50)
 seg_parser.add_argument('--val_interval', type=int, default=1000)
 seg_parser.add_argument('--iter_ckpt_interval', type=int, default=2000)
 args = seg_parser.parse_args()
@@ -68,9 +68,13 @@ with open(join(VOC_dump_home, 'parameters.txt'), 'w') as f:
     f.write('\n'.join(args.__str__()[10:-1].split(', ')))
 
 front_end = ResNet50(473, 473, 3, get_FCN, weight_decay)
+#model = deeplab_v3_ResNet50(front_end,  num_classes)
 model = PSPNet(front_end, num_classes)
-d_train, d_val = load_VOC(VOC_home, 21, resize_shape, num_train=20, num_val=10, data_set=False)
+#model = FCN(front_end,  32, num_classes)
 
+d_train, d_val = load_VOC(VOC_home, 255, resize_shape, num_train=12000, num_val=12000, data_set=False)
+
+is_train = True
 if is_train:
     commission_training_task(model, VOC_dump_home, d_train, d_val, learning_rate, lr_decay, momentum, nr_iter, batch_size,
                              report_interval, val_interval, iter_ckpt_interval)
@@ -79,3 +83,4 @@ if is_predict:
     X_val, y_val, y_mask_val, id_val = d_val
     y_pred, acc, mIoU = commission_predict(model, None, VOC_dump_home, X_val, y_val, y_mask_val)
     save_pred_results(VOC_palette, y_pred, id_val, join(VOC_dump_home, 'output_{}@{}'.format(acc, mIoU)))
+
